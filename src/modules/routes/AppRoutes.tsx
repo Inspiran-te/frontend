9b8727bb98d50fdfb7e2f34b559866eaf8937163
Sergei Shologin <s.shologin@yandex.ru>
2023-10-25 23:45:03 +0600

    Add public routes

---
+++ src/modules/routes/AppRoutes.tsx
@@ -0,0 +1,15 @@
+import { useRoutes } from 'react-router-dom'
+
+import { PrivateRoutes } from './PrivateRoutes'
+import { PublicRoutes } from './PublicRoutes'
+
+export const AppRoutes = () => {
+	//Todo Добавить auth (после аутенфикации отображается PrivateRoutes)
+	//const auth = false
+
+	//const routes = auth ? PrivateRoutes : PublicRoutes
+
+	const element = useRoutes([...(PublicRoutes)])
+	console.log('AppRoutes', PrivateRoutes)
+	return <>{element}</>
+}
