1aa4a210ee8116e8a0eb4b448e671268eccf8489
Sergei Shologin <s.shologin@yandex.ru>
2023-10-25 13:10:10 +0600

    React-TS-Redux-Styled-Prettier Template

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import 'big.js'
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.tsx</code> and save tol reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	)
+}
+
+export default App
