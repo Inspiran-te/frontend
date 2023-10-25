a2e46eca3c54cbf9e2f77678ad90ff7999d1a81f
Sergei Shologin <s.shologin@yandex.ru>
2023-10-25 21:46:17 +0600

    Create Redux store and Styled theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import 'big.js'
+import { store } from './redux/store'
+import { theme } from './theme/theme'
+import { Provider as ReduxProvider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.tsx</code> and save tol reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<>
+			<div>
+				<ReduxProvider store={store}>
+					<ThemeProvider theme={theme}>
+						<div />
+					</ThemeProvider>
+				</ReduxProvider>
+			</div>
+		</>
 	)
 }
 
